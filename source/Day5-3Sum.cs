a4256d086b83f148fc8fafd6fc951d7f3f258665
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-04-13 00:41:38 +0800

    Update Day5-3Sum.cs
    Add second dictionary still Time Limit Exceeded

--- source/Day5-3Sum.cs
+++ source/Day5-3Sum.cs
@@ -1,5 +1,4 @@
 public class Solution {
-//Time Limit Exceeded
     public IList<IList<int>> ThreeSum(int[] nums) {
         var result = new List<IList<int>>();
         if(nums.Length < 3)
@@ -14,21 +13,39 @@ public class Solution {
             else
                 dic[nums[index]] = new List<int>(index);
         }
-        
+        Dictionary<int, List<int>> secDictionary = new Dictionary<int,List<int>>();
         int i = 0;
         while(i < nums.Length - 2){
             var x = nums[i];
+            
+            if(secDictionary.TryGetValue(x, out List<int> secIndexList))
+            {
+                int secCount = 0;
+                var secListLength = secIndexList.Count();
+                while(secCount < secListLength - 1){
+                    if(secIndexList[0] > x && secIndexList[1] > x)
+                        result.Add(new List<int>(){i, secIndexList[0], secIndexList[1] });
+                    secCount++;
+                }                
+            }
             var j = i + 1;
             while(j < nums.Length -2)
             {
                 var y = nums[j];
+               
                 var targetValue = 0 - x - y;
                 if(dic.TryGetValue(targetValue, out List<int> indexList))
                 {
                     var count = 0;
-                    while(count < indexList.Count() - 1)
+                    var listLength = indexList.Count();
+                    while(count < listLength - 1)
                     {
-                        result.Add(new List<int>(){i, j, indexList[count]});
+                        if(indexList[count] > j)
+                        {
+                            result.Add(new List<int>(){i, j, indexList[count]});
+                            secDictionary[x] = new List<int>(){j, indexList[count]};
+                        }
+                        
                         count++;
                     }
                     
