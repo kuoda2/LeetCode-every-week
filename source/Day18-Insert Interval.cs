6fb791c75cabc39d400efa7c0e22631414783443
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-04-28 00:10:01 +0800

    Update Day18-Insert Interval.cs
    Runtime: 264 ms, faster than 21.72% of C# online submissions for Insert Interval.
    Memory Usage: 34 MB, less than 64.61% of C# online submissions for Insert Interval.
    O(n)

--- source/Day18-Insert Interval.cs
+++ source/Day18-Insert Interval.cs
@@ -1,62 +1,28 @@
 public class Solution {
     public int[][] Insert(int[][] intervals, int[] newInterval) {
-        int n = intervals.Length;
-        if(n == 0)
-            return new int[][]{newInterval};
-        
-        int overlapFrom = 0;
-        int overlapTo = 0;
-        int low = 0, high = n - 1;
-        int mid = 0;
-        while(low != high){
-            mid = (low + high) / 2;
-            if(intervals[mid][0] <= newInterval[0])
-            {
-                overlapFrom = mid;
-                low = mid + 1;              
-            }
-            else
-            {
-                high = mid;
-            }
-        }
-        low = 0;
-        high = n - 1;
-        while(low != high){
-            mid = (low + high) / 2;
-            if(intervals[mid][1] >= newInterval[1])
-            {
-                overlapTo = mid;
-                high = mid;
-            }
-            else
-            {
-                low = mid + 1;
-            }
-        }
-        if(intervals[overlapTo][1] <= newInterval[1])
-            intervals[overlapTo][1] = newInterval[1];
-        else if (intervals[overlapTo][0] >= newInterval[1])
-            intervals[overlapTo][0] = newInterval[1];
-        
-        if(intervals[overlapFrom][0] >= newInterval[0])
-            intervals[overlapFrom][0] = newInterval[0];
-        else if(intervals[overlapFrom][1] <= newInterval[0])
-            intervals[overlapFrom][1] = newInterval[0];
-        
-        int skipCount = overlapTo - overlapFrom - 1;
-        skipCount = skipCount > 0 ? skipCount : 0;
-        
-        Console.WriteLine(overlapFrom);
-        Console.WriteLine(overlapTo);
+    if(intervals.Length == 0)
+		return new int[][] { newInterval };
 
-        int[][] result = new int[n-skipCount][];
-        for(int i = 0, index = 0; i < n ; i++){
-            if(i > overlapFrom && i < overlapTo)
-                continue;
-            result[index] = intervals[i];
-            index++;
-        }
-        return result;
+	List<int[]> result = new List<int[]>();
+	foreach(var interval in intervals)
+	{
+		if(newInterval[0] > interval[1])
+		{
+			result.Add(interval);
+		}
+		else if(newInterval[1] < interval[0])
+		{
+			result.Add(newInterval);
+			newInterval = interval;
+		}
+		else
+		{
+			newInterval[0] = Math.Min(interval[0], newInterval[0]);
+			newInterval[1] = Math.Max(interval[1], newInterval[1]);
+		}
+	}
+    result.Add(newInterval);
+    return result.ToArray();
     }
 }
+  
