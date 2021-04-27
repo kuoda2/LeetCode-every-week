34b4c8706c11ab6245f9cc2850210e2d450c6d44
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-04-27 22:25:42 +0800

    Update Day15-Spiral Matrix.cs
    Wrong 2

--- source/Day15-Spiral Matrix.cs
+++ source/Day15-Spiral Matrix.cs
@@ -1,8 +1,8 @@
 public class Solution {
     public IList<int> SpiralOrder(int[][] matrix) {
         IList<int> result = new List<int>();
-        int[] mstep = new int[4]{1,0,-1,0};
-        int[] nstep = new int[4]{0,1,0,-1};
+        int[] mstep = new int[4]{0,1,0,-1};
+        int[] nstep = new int[4]{1,0,-1,0};
         int m = matrix.Length;
         int n = matrix[0].Length;
         int ml = 0;
@@ -16,10 +16,10 @@ public class Solution {
         int l = 0;
         m--;
         n--;
-        while(i < step){
+        while(i <= step){
             
       
-          
+          Console.WriteLine("m=>{0}, n=>{1}, ml=>{2}, nl=>{3}, l=>{4}",m,n,ml,nl,l);
                 Console.WriteLine(tempM.ToString() + " " + tempN.ToString());
                 result.Add(matrix[tempM][tempN]);
                 i++;
@@ -30,18 +30,18 @@ public class Solution {
                 if(tm > m || tn > n)
                 {
                     Console.WriteLine("tm=>{0}, tn=>{1}",tm,tn);
-                    nl += Math.Abs(mstep[l]);
+                    nl += Math.Abs(nstep[l]);
 
-                    m -= Math.Abs(nstep[l]);
+                    m -= Math.Abs(mstep[l]);
                     l++;
                     Console.WriteLine("l++");
                 }
                 else if(tm < ml || tn < nl){
                    Console.WriteLine("tm=>{0}, tn=>{1}",tm,tn);
                     Console.WriteLine("ml=>{0},nl=>{1}",ml,nl);
-                    n -= Math.Abs(mstep[l]);
+                    n -= Math.Abs(nstep[l]);
 
-                    ml += Math.Abs(nstep[l]);
+                    ml += Math.Abs(mstep[l]);
                     l++;
                     Console.WriteLine("l++");
 
@@ -49,11 +49,11 @@ public class Solution {
                 }
             if(l == 4)
                 l = 0;
-            Console.WriteLine("m=>{0}, n=>{1}, l=>{2}",m,n,l);
-
+            
             tempM += mstep[l];
             tempN += nstep[l];
             Console.WriteLine(tempM.ToString() + " " + tempN.ToString());
+            Console.WriteLine();
 
             }
         
