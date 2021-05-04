3bc3aad2b04b103ef36ceae0c0dbe15c20e3d413
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-05-04 23:36:49 +0800

    Update Day24-Decode Ways.cs
    Runtime: 88 ms, faster than 7.90% of C# online submissions for Decode Ways.
    Memory Usage: 26.5 MB, less than 5.16% of C# online submissions for Decode Ways.

--- source/Day24-Decode Ways.cs
+++ source/Day24-Decode Ways.cs
@@ -1,8 +1,37 @@
 public class Solution {
     public int NumDecodings(string s) {
+        var list = GetSecList(s);
+        var result = 1;
+        for(int i = 0; i < list.Count();i++){
+            result *= GetNumDecoding(list[i]);
+        }
+        return result;
         
-        int result = 0;
+   }
+    public List<string> GetSecList(string s){
+
+    int lastIndex = 0;
+    var result = new List<string>();
+    for(int i = 0; i < s.Length; i++){
+      if(s[i] == '1' || s[i] == '2'){
+        if(i != s.Length -1)
+          continue;
+        else
+        result.Add(s.Substring(lastIndex, i - lastIndex +1));
+      }
+      else{
+        result.Add(s.Substring(lastIndex, i- lastIndex + 1));
+        lastIndex = i+1;
+      }
+    }
+    return result;
+
+  }
+    
+    public static int GetNumDecoding(string s){
+          int result = 0;
         int lastResult = 1;
+        int lastAdd = 1;
         if(s == "0")
             return result;
         char lastChar = s[0];
@@ -24,12 +53,13 @@ public class Solution {
                 if(fitOne.Contains(currentChar)){
                     var temp = result;
                     result += lastResult;
+                    lastAdd = lastResult;
                     lastResult = temp;
                 }
                 else if(currentChar == '0'){
                     var temp = result;
                     if(result > 1)
-                        result -= lastResult;
+                        result -= lastAdd;
                     //lastResult = temp;
                 }
             }
@@ -37,12 +67,14 @@ public class Solution {
                 if(fitTwo.Contains(currentChar)){
                     var temp = result;
                     result += lastResult;
+                    lastAdd = lastResult;
+
                     lastResult = temp;
                 }
                 else if(currentChar == '0'){
                     var temp = result;
                     if(result > 1)
-                        result -= lastResult;
+                        result -= lastAdd;
                     //lastResult = temp;
                 }
             }
@@ -50,7 +82,7 @@ public class Solution {
                 return 0;
             lastChar = currentChar;
         }
-         return result;    
-   }
+         return result;   
+    }
 
 }
