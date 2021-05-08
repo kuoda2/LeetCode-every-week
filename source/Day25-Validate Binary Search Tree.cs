e9e902682d4b88a9621301b2ce0d0ade828e05c0
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-05-08 14:10:02 +0800

    Update Day25-Validate Binary Search Tree.cs
    Wrong
    ex : [1, 1]
    最頂層的沒檢核到

--- source/Day25-Validate Binary Search Tree.cs
+++ source/Day25-Validate Binary Search Tree.cs
@@ -13,25 +13,33 @@
  */
 public class Solution {
     public bool IsValidBST(TreeNode root) {
-       return Valid(root);
-    }
-    public bool Valid(TreeNode currNode){
         
-        if(CheckLeft(currNode)){
-            if(CheckRight(currNode))
+        if(Valid(root.left, root))
+            if(Valid(root.right, root))
+                return true;
+       return false;
+    }
+    public bool Valid(TreeNode currNode, TreeNode lastNode){
+        
+        if(CheckLeft(currNode, lastNode)){
+            if(CheckRight(currNode, lastNode))
                 return true;
         }
         return false;
     }
     
-    public bool CheckRight(TreeNode node){
-        if(node.left == null || node.val > node.left.val)
+    public bool CheckLeft(TreeNode node, TreeNode lastNode){
+        if(node == null || node.left == null)
             return true;
+        if(node.val > node.left.val && lastNode.val < node.left.val)
+                return true;
         return false;
     }
     
-    public bool CheckLeft(TreeNode node){
-          if(node.right == null || node.val < node.right.val)
+    public bool CheckRight(TreeNode node, TreeNode lastNode){
+        if(node == null || node.right == null)
+            return true;
+        if(node.val < node.right.val && lastNode.val > node.right.val)
             return true;
         return false;
     }
