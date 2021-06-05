f944523d767f945c14169babdf7d2c8775a97431
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-06-05 19:44:00 +0800

    Create Day51-Invert Binary Tree.cs
    Runtime: 120 ms, faster than 7.70% of C# online submissions for Invert Binary Tree.
    Memory Usage: 24.7 MB, less than 70.41% of C# online submissions for Invert Binary Tree.

---
+++ source/Day51-Invert Binary Tree.cs
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public TreeNode InvertTree(TreeNode root) {
+        if(root == null) return null;
+        TreeNode result = new TreeNode(root.val);
+        result = DFS(root, result);
+        return result;
+    }
+    
+    public TreeNode DFS(TreeNode node, TreeNode newNode){
+        if(node == null)
+            return node;
+        if(node.left != null)
+        newNode.right = new TreeNode(node.left.val);
+        DFS(node.left, newNode.right);
+        if(node.right != null)
+        newNode.left = new TreeNode(node.right.val);
+        DFS(node.right, newNode.left);
+        return newNode;
+    }
+}
