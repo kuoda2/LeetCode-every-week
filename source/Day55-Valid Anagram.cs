b9b77ca78ca4fca1b63b91a26fd6ef245db64a9e
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-07-10 17:38:12 +0800

    Update Day55-Valid Anagram.cs
    Runtime: 72 ms, faster than 95.65% of C# online submissions for Valid Anagram.
    Memory Usage: 24.4 MB, less than 73.80% of C# online submissions for Valid Anagram.

--- source/Day55-Valid Anagram.cs
+++ source/Day55-Valid Anagram.cs
@@ -2,23 +2,14 @@ public class Solution {
     public bool IsAnagram(string s, string t) {
         if(s.Length != t.Length)
             return false;
-        Dictionary<char,int> map = new Dictionary<char,int>();
+        int[] array = new int[26];
         for(int i = 0;i<s.Length; i++){
-            if(map.TryGetValue(s[i],out int val)){
-                map[s[i]]++;
-            }else{
-                map[s[i]] = 1;
-            }
+            array[s[i] - 'a']++;
+            array[t[i] - 'a']--;
         }
-        for(int i = 0;i<t.Length; i++){
-            if(map.TryGetValue(t[i],out int count)){
-                if(count < 1)
-                    return false;
-                map[t[i]]--;
-            }
-            else{
+        for(int i = 0; i < array.Length;i++){
+            if(array[i] != 0)
                 return false;
-            }
         }
         return true;
     }
