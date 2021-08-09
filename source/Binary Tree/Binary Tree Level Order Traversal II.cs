c92829e9b5d17c1a3cd8f41f39100ce26bbc6fb2
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-08-09 22:27:30 +0800

    Create Binary Tree Level Order Traversal II.cs
    Runtime: 248 ms, faster than 37.31% of C# online submissions for Binary Tree Level Order Traversal II.
    Memory Usage: 31.5 MB, less than 94.30% of C# online submissions for Binary Tree Level Order Traversal II.

---
+++ source/Binary Tree/Binary Tree Level Order Traversal II.cs
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left;
+ *     public TreeNode right;
+ *     public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+public class Solution {
+    public IList<IList<int>> LevelOrderBottom(TreeNode root) {
+        var dic = new Dictionary<int,List<int>>();
+        GetNodeVal(root, 0, dic);
+        var result = new List<IList<int>>();
+        var levelList = new List<int>(dic.Keys);
+        levelList.Reverse();
+        foreach(var item in levelList){
+            result.Add(dic[item]);
+        }
+        return result;
+    }
+    public void GetNodeVal(TreeNode node, int level, Dictionary<int,List<int>> dic){
+        if(node == null)
+            return;
+        if(!dic.ContainsKey(level))
+            dic[level] = new List<int>();
+        dic[level].Add(node.val);
+        level++;
+        GetNodeVal(node.left, level, dic);
+        GetNodeVal(node.right, level, dic);
+    }
+}
