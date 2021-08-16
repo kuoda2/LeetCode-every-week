1e56d2a2ce82b03f81737a7044095377a10fda94
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-08-16 23:56:47 +0800

    Update Day28-Maximum Depth of Binary Tree.cs
    Add iteration

--- source/Day28-Maximum Depth of Binary Tree.cs
+++ source/Day28-Maximum Depth of Binary Tree.cs
@@ -15,11 +15,17 @@ public class Solution {
     public int MaxDepth(TreeNode root) {
        return GetDepth(root, 0);
     }
-    
+    //Runtime: 84 ms, faster than 99.07% of C# online submissions for Maximum Depth of Binary Tree.
+    //Memory Usage: 26.1 MB, less than 50.34% of C# online submissions for Maximum Depth of Binary Tree.
     public int GetDepth(TreeNode node, int depth){
         if(node == null)
             return depth;
         depth++;
         return Math.Max(GetDepth(node.left, depth), GetDepth(node.right, depth));
+    }
+    //Runtime: 88 ms, faster than 95.92% of C# online submissions for Maximum Depth of Binary Tree.
+    //Memory Usage: 26 MB, less than 50.34% of C# online submissions for Maximum Depth of Binary Tree.
+    public int GetDepthIteration(TreeNode root){
+        return root == null ? 0 :  (1 + Math.Max(GetDepthIteration(root.left), GetDepthIteration(root.right)));
     }
 }
