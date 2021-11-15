ac79aa607b531d45435aa7f7656c546e978c99ae
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2021-11-15 20:45:16 +0800

    Update Climbing Stairs.cs

--- source/Dynamic programming/Climbing Stairs.cs
+++ source/Dynamic programming/Climbing Stairs.cs
@@ -12,3 +12,19 @@ public class Solution {
         return dp[n];
     }
 }
+
+public class Solution2 {
+    public int ClimbStairs(int n) {
+        int[] dp = new int[n+1];
+        dp[0] = 1;
+        var maxStep = 2;
+        
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= maxStep; j++){
+                if(i - j >= 0)
+                    dp[i] += dp[i-j];
+            }
+        }
+        return dp[n];
+    }
+}
