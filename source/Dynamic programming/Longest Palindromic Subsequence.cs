b0b2f75b560caa3d51fb9ad1bb6a79ba1fb31068
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2022-01-25 12:48:55 +0800

    Create Longest Palindromic Subsequence.cs
    Runtime: 142 ms, faster than 46.78% of C# online submissions for Longest Palindromic Subsequence.
    Memory Usage: 45.2 MB, less than 35.09% of C# online submissions for Longest Palindromic Subsequence.
    要記得初始化

---
+++ source/Dynamic programming/Longest Palindromic Subsequence.cs
@@ -0,0 +1,22 @@
+public class Solution {
+    public int LongestPalindromeSubseq(string s) {
+        var dp = new int[s.Length][];
+        for(int i = 0; i < s.Length; i++){
+            dp[i] = new int[s.Length];
+        }
+        
+        for(int i = s.Length -1; i >=0; i--){
+            dp[i][i] = 1; // 初始化
+            for(int j = i + 1; j < s.Length; j++)
+            {
+                if(s[i] == s[j]){
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                }
+                else{
+                    dp[i][j] = Math.Max(dp[i+1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[0][^1];
+    }
+}
