7f26afee51486056cc52cef69117f769b7c616ca
Chun Te, Kuo <kuoda2@users.noreply.github.com>
2022-03-14 23:15:06 +0800

    Update House Robber II.cs
    Runtime: 76 ms, faster than 94.22% of C# online submissions for House Robber II.
    Memory Usage: 38.4 MB, less than 5.95% of C# online submissions for House Robber II.

--- source/Dynamic programming/House Robber II.cs
+++ source/Dynamic programming/House Robber II.cs
@@ -18,4 +18,28 @@ public class Solution {
         }
         return dp[high];
     }
+    public int Rob2(int[] nums) {
+        if(nums.Length == 1)
+            return nums[0];
+        
+        if(nums.Length == 2)
+            return Math.Max(nums[0], nums[1]);
+        var result = Math.Max(Rob(nums, 0, nums.Length - 2), Rob(nums, 1, nums.Length - 1));
+        return result;
+        
+    }
+    
+    private int Rob2(int[] nums, int left, int right){
+        if(left == right)
+            return nums[left];
+        
+        var dp = new int[nums.Length];
+        dp[left] = nums[left];
+        dp[left + 1] = Math.Max(nums[left], nums[left + 1]);
+        
+        for(int i = left + 2; i <= right; i++){
+            dp[i] = Math.Max(dp[i - 1], dp[i - 2] + nums[i]);
+        }
+        return dp[right];
+    }
 }
